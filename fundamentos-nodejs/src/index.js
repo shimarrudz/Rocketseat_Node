21cf16efa8edecdb367519b9ab2edc2ce4a7026d
shimarrudz <vic.shima.vss@gmail.com>
2023-06-24 20:46:04 -0300

    "Using Nodemon!"

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -5,10 +5,11 @@ const app = express();
 // Request é o que vamos receber da requisição
 // É o que vai retornar da requisição
 app.get("/",  (request, response) => {
-    return response.json({message: "Hello World Ignite!"}); // Envia uma mensagem para quem está solicitando
+    return response.json({message: "Hello World Ignite! - Fundamentos NodeJS"}); // Envia uma mensagem para quem está solicitando
 });
 
 // .listen é usado para passar a porta da aplicação como parâmetro
 // localhost: 3333
 app.listen(3333);
+
 
