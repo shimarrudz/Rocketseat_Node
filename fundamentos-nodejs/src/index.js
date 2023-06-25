b78cc9d98df218df22af8678daaa142e3ca3c379
shimarrudz <vic.shima.vss@gmail.com>
2023-06-25 13:34:57 -0300

    first class finished!

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,14 +2,60 @@ const express = require('express');
 
 const app = express();
 
-// Request é o que vamos receber da requisição
-// É o que vai retornar da requisição
-app.get("/",  (request, response) => {
-    return response.json({message: "Hello World Ignite! - Fundamentos NodeJS"}); // Envia uma mensagem para quem está solicitando
+app.use(express.json);
+
+/*
+ * GET - Buscar uma informação dentro do servidor 
+ * POST - Inserir informações no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+*/
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação, Filtro = /courses?page=1&order=esc
+ ******* O query params é opcional, diferente do route params
+ * Body Params => Os objetos inserção/alteração (JSON)
+
+*/
+
+app.get("/courses",  (request, response) => {
+    const query = request.query;
+    console.log(query);
+    return response.json(["Curso 1", "Curso 2", "Curso 3"
+        ]);
+    });
+
+app.post("/courses",  (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"
+        ]);
+    });
+
+app.put("/courses/:id", (request, response) => {
+    const params = request.params;
+    console.log(params)
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
-// .listen é usado para passar a porta da aplicação como parâmetro
-// localhost: 3333
-app.listen(3333);
+app.patch("/courses/:id",  (request, response) => {
+    const params = request.params;
+    console.log(params)
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"
+        ]);
+    });
 
+/* app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+}) */
+
+/* app.delete("/courses/:id" , (request, response) => {
+    return response.json(["Curso 6", "Curso 7", "Curso 4"]);
+}) */
+
+app.listen(3333);
 
